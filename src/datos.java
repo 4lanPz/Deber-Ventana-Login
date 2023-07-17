d6b7e7108ba06c5f63e3eac30ab04cdeb61d9b11
Alan <alanstvn420@gmail.com>
2023-07-17 13:06:15 -0500

    Alan

--- src/datos.java
+++ src/datos.java
@@ -1,2 +1,98 @@
-package PACKAGE_NAME;public class datos {
-}
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+public class datos {
+    int con1=0;
+    String Usuario,Pasword , A1, A2;
+    public JPanel pantalla;
+    private JTextField TUsuario;
+    private JTextField TPassword;
+    private JButton guardardatosButton;
+    private JLabel LTitulo;
+    private JLabel LUsuario;
+    private JLabel LPassword;
+    private JLabel LContador;
+    private JButton BLogin;
+    private JButton cargar;
+    private JTextField textField1;
+    private JTextField textField2;
+
+    public datos(String usuario, String pasword) {
+        Usuario = usuario;
+        Pasword = pasword;
+    }
+    public String getUsuario() {
+        return Usuario;
+    }
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+    public String getPasword() {
+        return Pasword;
+    }
+    public void setPasword(String pasword) {
+        Pasword = pasword;
+    }
+
+    public datos() {
+        //la accion de el Jtextfield que va a obtener los datos de texto que
+        //se ingrese en el programa y se va a almacenar en una variable
+        TUsuario.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Usuario=TUsuario.getText();
+            }
+        });
+        //la accion de el Jtextfield que va a obtener los datos de texto que
+        //se ingrese en el programa y se va a almacenar en una variable
+        TPassword.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pasword=TPassword.getText();
+            }
+        });
+        /*En este caso se crea el boton que al dar click va a
+        guardar un archivo.dat que va a contener los datos que se
+        ingresaron en la parte anterior y nos da un error si esque no se pudo*/
+        guardardatosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                con1++;
+                LContador.setText("Se ha ingresado: "+con1+" Usuarios");
+                File file= new File("C://Users//alans//Documents//archivo.dat");
+                try {
+                    FileWriter fileWriter = new FileWriter(file, true);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                    bufferedWriter.write(Usuario+":"+Pasword+"\n");
+                    bufferedWriter.close();
+                } catch(IOException ex) {
+                    System.out.println("Error al escribir en el archivo '" + file + "'");
+                }
+            }
+        });
+        /* en este caso, este boton va a abrir el otro JPanel en donde se va a loguear
+         el usuario, osea se va a ingresar los datos que se ingresaron antes y se
+         comparan para comprobar de si existen*/
+        BLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new JFrame("login");
+                frame.setContentPane(new login().pantallal);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+    /*main que tiene el ejecutable para que inicie el programa y el JPanel*/
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("datos");
+        frame.setContentPane(new datos().pantalla);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    }
+
--- src/login.java
+++ src/login.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class login {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+//se crean todos los botones del JPanel
+public class login {
+    String Usuario, Contra;
+    public JPanel pantallal;
+    private JTextField TUsuario;
+    private JTextField TContra;
+    private JButton BInicio;
+    private JLabel LTitulo;
+    private JLabel LUsuario;
+    private JLabel LContra;
+    private JLabel Verificacion;
+
+    public login() {
+        /*se crea el codigo de los botones que van a tener los valores que se
+        * ingresa por teclado que van a compararse para entrar dar correcto
+        al login*/
+        TUsuario.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Usuario=TUsuario.getText();
+            }
+        });
+        TContra.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Contra=TContra.getText();
+            }
+        });
+        /*Este boton lo que hace es abrir el archivo .dat que tiene la informacion del otro
+         JPanel, en este caso se lo usa para verificar los datos que se ingreso antes estan
+         dentro de ese archivo, si los datos ingresados si se encuentran dentro, nos vamos
+         a logear correctamente y nos muestra un texto indicando eso*/
+        BInicio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //cambiar la direccion a la misma direccion de donde se guarda los datos
+                File file= new File("C://Users//alans//Documents//archivo.dat");
+                boolean found = false;
+                String line = null;
+                try {
+                    FileReader fileReader = new FileReader(file);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+                    String Verifi = Usuario+":"+Contra;
+                    while((line = bufferedReader.readLine()) != null) {
+                        if(line.equals(Verifi)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    bufferedReader.close();
+                } catch(FileNotFoundException ex) {
+                    System.out.println("No se pudo abrir el archivo '" + file + "'");
+                } catch(IOException ex) {
+                    System.out.println("Error al leer el archivo '" + file + "'");
+                }
+                if(found) {
+                    Verificacion.setText("Bienvenido "+Usuario);
+                } else {
+                    Verificacion.setText("<html>¡¡DATOS INCORRECTOS!!<br>¡¡Verifique los datos!!<html>");
+                }
+            }
+        });
+    }
 }
